5754d78e69cee7f2790ed6cfc90095b5f38fedfd
klittle314 <klittle@iecodesign.com>
2026-04-17 08:33:10 -0500

    correct logic for subtitles

--- scripts/helper.R
+++ scripts/helper.R
@@ -6,25 +6,34 @@ run_chart_mean <- function(data = df,
                            title,
                            ylab,
                            y_ref) {
+ 
+  mean_use <- round(colMeans(df[,var_y], na.rm = TRUE),1)
+   
+  if(!systolic && !pulse) {
+    ylab = "Pulse Pressure mm HG"
+    subtitle = paste0("Pulse Pressure avg = ", mean_use)
+  }
   
   if(systolic){
     ylab <- "Systolic mm HG"
+    subtitle <- paste0("Systolic avg = ", mean_use)
   } else {
     ylab <- "Diastolic mm HG"
+    subtitle <- paste0("Diastolic avg = ", mean_use)
   }
   
   if(pulse) {
     ylab = "Pulse BPM"
+    subtitle = paste0("Pulse BPM = ", mean_use)
   }
   
-  if(!systolic && !pulse) {
-    ylab = "Pulse Pressure mm HG"
-  }
+  
   
   p_out <- ggplot(data = data, aes(y = !!sym(var_y), x= Date))+
                   theme_bw() +
     geom_point(size = rel(point_size)) +
     labs(title = paste0(var_y),
+         subtitle = subtitle,
          y = ylab) +
     geom_hline(yintercept = y_ref, linetype = "dashed")
   
